dac88a43d68acbcaaf1a4c7507d2e9f8d9e9c53e
TobinStonelake <tobinstonelake@outlook.com>
2020-09-30 12:26:57 +0000

    Changed typo on script link

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-function getData() {
+function getData(cb) {
   var xhr = new XMLHttpRequest();
 
   xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
   xhr.send();
 
-  xhr.onreadystatechange = function () {
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       cb(JSON.parse(this.responseText));
     }
@@ -13,6 +13,9 @@ function getData() {
 
 function printDataToConsole(data) {
     console.log(data);
-}
+};
 
 getData(printDataToConsole);
+
+// Roman - added to test
+console.log('test')
